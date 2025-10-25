6436833b6eafbfab02d76d46cb6577fdf7f7aace
matee8 <graves-bluff-pesky@duck.com>
2025-10-25 21:09:22 +0200

    feat(scripts): add main CLI entry point

---
+++ src/webscraper/_scripts/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
